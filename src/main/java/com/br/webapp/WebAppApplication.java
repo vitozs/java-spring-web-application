5a535a4996462dab99fa5f04bef22a96fc97e60d
vitozs <vitornuneschagas2016@gmail.com>
2023-10-03 01:42:51 -0300

    Ajustado o main

--- src/main/java/com/br/webapp/WebAppApplication.java
+++ src/main/java/com/br/webapp/WebAppApplication.java
@@ -2,9 +2,12 @@ package com.br.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebAppApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
